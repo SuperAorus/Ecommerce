1ed8ff0daa42293b8f82627b833d4928375545be
Carlos Rodriguez <carlosdrg06@gmail.com>
2022-10-16 11:27:27 -0600

    Update script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const bar = document.getElementById('bar');
-const nav = document.getElementsById('navbar');
-
-if (bar) {
-    bar.addEventListener('click', () => {
-        nav.classlist.add(active);
-    })
-}
